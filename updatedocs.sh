0fe0c2304fbbac8ba1c5795833769cd7ce0fdb72
Papa Jessie <githubpapajessie@tenebreuse.org>
2018-11-25 17:01:55 +0100

    Add more details to commit logs

--- updatedocs.sh
+++ updatedocs.sh
@@ -2,8 +2,11 @@
 git pull origin
 ./swc.py mode=docs
 
+a=$(cat lastversion.txt)
+
+a=$(( a+1-1 ))
 if [ -n "$1" ]; then
     git add docs
-    git commit -m 'Update docs'
+    git commit -m "Update docs ${a}"
     git push origin
 fi
